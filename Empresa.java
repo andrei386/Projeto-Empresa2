2c34dc764ee7f64a4255c0ee920044dcfeca3068
andrei386 <andreivcnt@gmail.com>
2025-02-21 15:55:27 -0300

    commit inicial

--- Empresa.java
+++ Empresa.java
@@ -1,7 +1,7 @@
 ﻿import java.util.*;
 import java.lang.Math;
 
-class Program {
+class ProjetoEmpresa {
     private static Scanner input = new Scanner(System.in);
 
     public static void main(String[] args) {
